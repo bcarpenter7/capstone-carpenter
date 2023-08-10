d28e2b179108f2e03934bc5fd39b1c60eca4f506
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-08-10 14:30:57 -0400

    tailwinding

--- client/src/components/RegisterPage/RegisterPage.jsx
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -33,7 +33,7 @@ function handleSubmit(e){
     } else {
         console.log("username is unique, creating a new account")
         handleCreateUser(cred)
-        // setUser(cred)
+        setUser(cred)
         document.getElementById('errorMsgTwo').style.opacity = 0;
     }
 }
@@ -41,21 +41,35 @@ function handleSubmit(e){
 if(!accountExists){
     return (
         <>
-        <div className="homeDiv">
+        <div className="p-8 bg-slate-200 rounded mx-auto mt-20 max-w-[80vmin] px-4 md:px-8 2xl:px-16 md:w-full lg:w-3/5 2xl:w-4/6 flex h-full ltr:md:ml-7 rtl:md:mr-7 flex-col ltr:lg:pl-7 rtl:lg:pr-7">
 
-        <h1>Register Here</h1>
-        <form className="form" onSubmit={handleSubmit}>
-            <label>Username:</label>
-            <input placeholder="username" onChange={handleChange} name="username"></input>
-            <label>Password:</label>
-            <input placeholder="password" onChange={handleChange} name="password"></input>
-            <label>First Name</label>
-            <input placeholder="password" onChange={handleChange} name="firstName"></input>
-            <label>Last Name:</label>
-            <input placeholder="password" onChange={handleChange} name="lastName"></input>
+        <h4 class="text-2xl 2xl:text-3xl font-bold text-heading mb-10">
+              Register Here
+        </h4>
+        <form className="w-full mx-auto flex flex-col justify-center" onSubmit={handleSubmit}>
+            <label className="block text-gray-600 font-semibold text-sm leading-none mb-3 cursor-pointer">Username:</label>
+            <input className="mb-3 py-2 px-4 md:px-5 w-full appearance-none transition duration-150 ease-in-out border text-input text-xs lg:text-sm font-body placeholder-body min-h-12 transition duration-200 ease-in-out bg-white border-gray-300 focus:outline-none focus:border-heading h-11 md:h-12" 
+            placeholder="Username" onChange={handleChange} name="username"></input>
+            
+            <label className="block text-gray-600 font-semibold text-sm leading-none mb-3 cursor-pointer">Password:</label>
+            <input className="mb-3 py-2 px-4 md:px-5 w-full appearance-none transition duration-150 ease-in-out border text-input text-xs lg:text-sm font-body placeholder-body min-h-12 transition duration-200 ease-in-out bg-white border-gray-300 focus:outline-none focus:border-heading h-11 md:h-12"
+            placeholder="Password" onChange={handleChange} name="password"></input>
+
+            <label className="block text-gray-600 font-semibold text-sm leading-none mb-3 cursor-pointer">First Name</label>
+            <input className="mb-3 py-2 px-4 md:px-5 w-full appearance-none transition duration-150 ease-in-out border text-input text-xs lg:text-sm font-body placeholder-body min-h-12 transition duration-200 ease-in-out bg-white border-gray-300 focus:outline-none focus:border-heading h-11 md:h-12"
+            placeholder="First Name" onChange={handleChange} name="firstName"></input>
+
+            <label className="block text-gray-600 font-semibold text-sm leading-none mb-3 cursor-pointer">Last Name:</label>
+            <input className="mb-3 py-2 px-4 md:px-5 w-full appearance-none transition duration-150 ease-in-out border text-input text-xs lg:text-sm font-body placeholder-body min-h-12 transition duration-200 ease-in-out bg-white border-gray-300 focus:outline-none focus:border-heading h-11 md:h-12"
+            placeholder="Last Name" onChange={handleChange} name="lastName"></input>
             <h1 id="errorMsgTwo" className="hidden">An account already exists with this username.</h1>
-            <button onClick={handleSubmit}>Login</button>
-            <button onClick={handleExists}>Already have an account? Click to log in</button>
+            <button onClick={handleSubmit}
+            className="text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none  bg-black text-white px-5 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 mt-1 text-sm lg:text-base w-full sm:w-auto"
+            
+            >Register</button>
+            <button 
+            className="text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none  bg-black text-white px-5 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 mt-1 text-sm lg:text-base w-full sm:w-auto"
+            onClick={handleExists}>Already have an account? Click to log in</button>
         </form>
         </div>
        
