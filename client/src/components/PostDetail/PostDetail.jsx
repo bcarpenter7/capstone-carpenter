975c303e4547189a2657ccbc03ca8d7c5961dcdc
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-08-11 21:43:13 -0400

    detail fix

--- client/src/components/PostDetail/PostDetail.jsx
+++ client/src/components/PostDetail/PostDetail.jsx
@@ -37,25 +37,32 @@ export default function PostDetail({article, handleEdit, setPage, deletePost, ha
             
                 
                 <div className="p-8 bg-slate-200 rounded mx-auto mt-20 max-w-[80vmin] px-4 md:px-8 2xl:px-16 md:w-full lg:w-3/5 2xl:w-4/6 flex h-full ltr:md:ml-7 rtl:md:mr-7 flex-col ltr:lg:pl-7 rtl:lg:pr-7">
-                    <div>
-                    <h1 className="text-2xl 2xl:text-3xl font-bold text-heading mb-10"> {article.title} </h1>
+                    <div className="mb-3">
+                        <h1 className="text-2xl 2xl:text-3xl font-bold text-heading mb-3"> {user.firstName} {user.lastName} </h1>
+                        <h3 className="block text-gray-600 font-semibold text-sm leading-none mb-3"> Published { article.updatedAt.slice(0, 10) ? article.createdAt.slice(0, 10) : article.updatedAt.slice(0, 10)}</h3>
+                    
                     </div>
-                    <h3 className="block text-gray-600 font-semibold text-sm leading-none mb-3"> {article.author} </h3>
-                    <h3 className="block text-gray-600 font-semibold text-sm leading-none mb-3"> Published { article.updatedAt.slice(0, 10) ? article.createdAt.slice(0, 10) : article.updatedAt.slice(0, 10)}</h3>
+                    {/* <h3 className="block text-gray-600 font-semibold text-sm leading-none mb-3"> {article.author} </h3> */}
                     
                     <div>
-                        <img className="max-w-[20vmin]" src={article.img} /> 
+                        <img className="max-width-2 mb-5" src={article.img} /> 
                     </div>
                     <div>
-                        <br />
-                        <br />
-                        <p className="articleText">{article.content}</p>
+                        <p className="previewText mb-5">{article.content}</p>
 
-                        <button name={article._id} onClick={editPost}> Click to edit</button>
-                        <button name={article._id} onClick={deletePost}>Click to delete</button>
-                      
-                        <br />
-                        <br />
+
+
+
+
+
+                        <div className="flex spaceBetween">
+                            <button className="rounded-full text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none bg-sky-500 text-white px-5 md:px-6 lg:px-6 py-4 md:py-3.5 lg:py-2 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 mt-1 text-sm lg:text-base w-full sm:w-auto"
+                            name={article._id} onClick={editPost}>Click to edit</button>
+
+                            <button className="rounded-full text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none  bg-red-500 text-white px-5 md:px-6 lg:px-6 py-4 md:py-3.5 lg:py-2 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 mt-1 text-sm lg:text-base w-full sm:w-auto"
+                            name={article._id} onClick={deletePost}>Click to delete</button>
+                        </div>
+                     
                     </div>
                 </div>
     
