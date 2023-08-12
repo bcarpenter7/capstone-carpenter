cba5f60b540f818b832d11de9fb496692f76718e
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-08-11 20:52:46 -0400

    styling

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -26,8 +26,6 @@ export default function Post({
         _id: user._id
     })
 
-
-
     function handleChange(e){
         console.log(e.target.name)
         setCurrentArticle(e.target.name)
@@ -59,26 +57,33 @@ export default function Post({
     return (
         <>
         <div className="p-8 bg-slate-200 rounded mx-auto mt-20 max-w-[80vmin] px-4 md:px-8 2xl:px-16 md:w-full lg:w-3/5 2xl:w-4/6 flex h-full ltr:md:ml-7 rtl:md:mr-7 flex-col ltr:lg:pl-7 rtl:lg:pr-7">
-        <h1 className='title1'>Explore</h1>
+        <h1 className='text-2xl 2xl:text-3xl font-bold text-heading mb-10 self-center'>Explore</h1>
         { reversed.map((p, idx) => (
 
             <>
-            <div className="blogCard">
+            <div className="hover:bg-blue-300 border-blue-300 border-2 rounded p-3 mb-1" >
            
                 <div>
-                    <h4 className="text-2xl 2xl:text-3xl font-bold text-heading mb-10">
-                        {allUsers.find(e => e._id === p.creatorId) ? `${allUsers.find(e => e._id === p.creatorId).firstName} ${allUsers.find(e => e._id === p.creatorId).lastName}` : 'Unknown User'}
-                        
-                    </h4>
-                    <span className="date">{ 
-                        p.updatedAt.slice(0, 10) ? p.createdAt.slice(0, 10) : p.updatedAt.slice(0, 10)
-                        }</span>
-                    <h3 className="previewText">{p.content}</h3>
-                    <button className="rounded-full text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none  bg-black text-white px-5 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 mt-1 text-sm lg:text-base w-full sm:w-auto"
-                    name={p._id} onClick={handleChange}>Click to View</button>
-                        {/* user.friendsList.includes(String(p.creatorId)) */}
+                    <div className="flex spaceBetween">
+                        <div className="flex-col mr-15">
+                            <h4 className="text-2xl 2xl:text-3xl font-bold text-heading mb-1 ">
+                                {allUsers.find(e => e._id === p.creatorId) ? `${allUsers.find(e => e._id === p.creatorId).firstName} ${allUsers.find(e => e._id === p.creatorId).lastName}` : 'Unknown User'}
+                                
+                            </h4>
+                            <span className="font-light">{ 
+                                p.updatedAt.slice(0, 10) ? p.createdAt.slice(0, 10) : p.updatedAt.slice(0, 10)
+                                }
+                            </span>
+                        </div>
+                        <div>
+                            <img className="max-w-xs max-h-30 rounded ml-20 mb-5" src={p.img} /> 
+                        </div>
+                    </div>
 
-               
+                    <h3 className="previewText">{p.content}</h3>
+                    <button className="mt-3 mr-2 rounded-full text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none  bg-black text-white px-5 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 text-sm lg:text-base w-full sm:w-auto"
+                    name={p._id} onClick={handleChange}>Click to View</button>
+
                                 { temp.friendsList.includes(String(p.creatorId)) ? (
                                     <button className="rounded-full text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none  bg-sky-500 text-white px-5 md:px-6 lg:px-6 py-4 md:py-3.5 lg:py-2 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 mt-1 text-sm lg:text-base w-full sm:w-auto"
                                      name={p.creatorId} onClick={handleRemoveFriend}>Following</button>
@@ -88,14 +93,8 @@ export default function Post({
                                 )
                                 }
                   
+                </div>
                 
-                    
-                 
-                   
-                </div>
-                <div>
-                    <img className="img" src={p.img} /> 
-                </div>
             </div>
 
             </>
